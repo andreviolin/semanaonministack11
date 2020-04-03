fde1508b5e1e65126b961bb984913bc68fe6b971
Andre <andreprojeto123@gmail.com>
2020-04-03 01:52:52 -0300

    Frontend da Aplicação

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {FiLogIn} from 'react-icons/fi';
 
+//impede que o react seja reiniciado quando apertar em um link
+import { Link, useHistory} from 'react-router-dom';
+
+//import Api
+import api from '../../services/api';
 
 import './styles.css';
 
@@ -8,22 +13,50 @@ import logoImg from '../../assets/logo.svg';
 import heroeImg from '../../assets/heroes.png';
 
 export default function Logon(){
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+    async function handleLogin(e){
+         e.preventDefault();
+
+
+         try {
+             if (id===""){
+                alert("O campo esta vazio!!!")
+             }else{
+                const response = await api.post('session', { id });
+
+                localStorage.setItem('ongId', id);
+                localStorage.setItem('ongName', response.data.name);
+
+                history.push('/profile');
+             }
+            
+           // console.log(response.data.name);
+         } catch (err) {
+            alert("falha tente novamente");
+         }
+    }
+
     return(
         <div className="logon-container">
             <section className="form">
                 <img src={logoImg} alt="Be the Hero"/>
             
 
-             <form>
+             <form onSubmit={handleLogin}>
                 <h1>Faça seu logon</h1>
 
-                <input placeholder="Sua ID"/>
+                <input placeholder="Sua ID"
+                value={id}
+                onChange={e => setId(e.target.value)}
+                />
                 <button className="button" type="submit">Entrar</button>
 
-                <a href="/register">
+                <Link to="/register">
                     <FiLogIn size={16} color="#E02041"/>
                     Não tenho cadastro
-                </a>
+                </Link>
              </form>
             </section> 
 
---
+++ frontend/src/routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Logon from './pages/Logon';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident';
+
+//usasse o "exact" para que execute a pagina seguinte, se não sempre ira visualizar a primeira rota
+export default function Routes(){
+    return(
+        <BrowserRouter>
+            <Switch>
+                
+                <Route path="/" exact component={Logon}/>
+                <Route path="/register" component={Register}/>
+                <Route path="/profile" component={Profile}/>
+                <Route path="/incidents/new" component={NewIncident}/>
+            </Switch>
+        </BrowserRouter>
+    );
+}
